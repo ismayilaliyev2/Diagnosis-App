00e44cbc55b01feb353b66d77ba675ff24e5487e
Aliyev Ismayil <aaa1122@gmail.com>
2024-08-03 18:53:44 +0400

    first commit

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ scoring_system = {
     # Define your scoring system here
 }
 
+questions = [
+    "input1", "input2", "input3", "input4", "input5",
+    "input6", "input7", "input8", "input9", "input10",
+    "input11", "input12", "input13", "input14", "input15",
+    "input16", "input17", "input18", "input19", "input20"
+]
+
 @app.route('/')
 def index():
     return render_template_string('''
@@ -73,40 +80,28 @@ def index():
         <div class="container">
             <h1>Select inputs to calculate their score:</h1>
             <form action="/page1" method="post">
-                <input type="hidden" name="input1" id="input1">
-                <input type="hidden" name="input2" id="input2">
-                <input type="hidden" name="input3" id="input3">
-                <input type="hidden" name="input4" id="input4">
-                <input type="hidden" name="input5" id="input5">
-                <input type="hidden" name="input6" id="input6">
-                <input type="hidden" name="input7" id="input7">
-                <input type="hidden" name="input8" id="input8">
-                <input type="hidden" name="input9" id="input9">
-                <input type="hidden" name="input10" id="input10">
-                <input type="hidden" name="input11" id="input11">
-                <input type="hidden" name="input12" id="input12">
-                <input type="hidden" name="input13" id="input13">
-                <input type="hidden" name="input14" id="input14">
-                <input type="hidden" name="input15" id="input15">
-                <input type="hidden" name="input16" id="input16">
-                <input type="hidden" name="input17" id="input17">
-                <input type="hidden" name="input18" id="input18">
-                <input type="hidden" name="input19" id="input19">
-                <input type="hidden" name="input20" id="input20">
+                <ul>
+                    {% for question in questions %}
+                        <li>
+                            <label>{{ question }}</label>
+                            <input type="text" name="{{ question }}">
+                        </li>
+                    {% endfor %}
+                </ul>
                 <button type="submit" class="button">Calculate</button>
                 <button type="submit" formaction="/save" class="button">Save</button>
             </form>
         </div>
     </body>
     </html>
-    ''')
+    ''', questions=questions)
 
 @app.route('/page1', methods=['POST'])
 def page1():
-    for i in range(1, 21):
-        session[f'input{i}'] = request.form.get(f'input{i}')
+    for question in questions:
+        session[question] = request.form.get(question)
     
-    inputs = [session.get(f'input{i}', '') for i in range(1, 21)]
+    inputs = [session.get(question, '') for question in questions]
     total_score = sum(scoring_system.get(input, 0) for input in inputs)
     
     return render_template_string('''
@@ -163,10 +158,10 @@ def page1():
 
 @app.route('/save', methods=['POST'])
 def save():
-    for i in range(1, 21):
-        session[f'input{i}'] = request.form.get(f'input{i}')
+    for question in questions:
+        session[question] = request.form.get(question)
     
-    inputs = [session.get(f'input{i}', '') for i in range(1, 21)]
+    inputs = [session.get(question, '') for question in questions]
     
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
